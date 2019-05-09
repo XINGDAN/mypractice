ff873a56ee0146ecc58f7742163c2800b7a0608e
XINGDAN <2772475450@qq.com>
2019-05-09 18:04:13 +0800

    Update login_lianxi_request.py

--- login_lianxi_request.py
+++ login_lianxi_request.py
@@ -1,9 +1,9 @@
 import pytest
 import requests
 
-@pytest.mark.parametrize("username,password",[(18715626526,123456)])
+@pytest.mark.parametrize("username,password",[(18700000000,123456)])
 def test_xinshetong_login(username,password):
-    url = 'https://www.xinshetong.com/apis/login'
+    url = 'https://www.xst.com/apis/login'
     myheaders = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0','Conternt-Type':'application/x-www-form-urlencoded'}
     payload = {'username':str(username),'password':str(password),'vcode':'','customerType':'enterprise'}
     res = requests.post(url,headers=myheaders,verify=False,data=payload)
