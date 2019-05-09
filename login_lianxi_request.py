6f6601e7473861f9db749ca4c4626e8d423c419d
xingdan <2772475450@qq.com>
2019-05-09 17:58:08 +0800

    完善登å½第第一个request登实战脚本

--- login_lianxi_request.py
+++ login_lianxi_request.py
@@ -1,8 +1,17 @@
 import pytest
 import requests
 
-def test_xinshetong_login():
-    url = ''
-    myheaders = {}
-    payload = {}
-    res = requests.post(url,headers=myheaders,param=payload,verify=False,data=)
+@pytest.mark.parametrize("username,password",[(18715626526,123456)])
+def test_xinshetong_login(username,password):
+    url = 'https://www.xinshetong.com/apis/login'
+    myheaders = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0','Conternt-Type':'application/x-www-form-urlencoded'}
+    payload = {'username':str(username),'password':str(password),'vcode':'','customerType':'enterprise'}
+    res = requests.post(url,headers=myheaders,verify=False,data=payload)
+
+    print(res.status_code)
+
+
+    tishi = (res.json().get("msg"))
+    sussess_yanzhen = (res.json().get("success"))
+    assert tishi == "登录成功"
+    #assert sussess_yanzhen == "true"
